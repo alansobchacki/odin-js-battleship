15c9aee2af4f119e72f7735571baf400a30e92de
Alan Sobchacki <a.sobchack@gmail.com>
2024-07-08 13:17:29 -0300

    fix an ESlint oversight

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,10 @@ import pluginJs from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier";
 
 export default [
-  { files: ["**/*.js"], languageOptions: { sourceType: "script" } },
+  {
+    files: ["**/*.js"],
+    languageOptions: { sourceType: "module", ecmaVersion: "latest" },
+  },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   eslintConfigPrettier,
---
+++ src/ship.js
@@ -0,0 +1,9 @@
+class Ship {
+  constructor(size) {
+    this.size = size;
+  }
+}
+
+const battleship = Ship.new(2);
+
+export default battleship;
